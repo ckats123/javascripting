09cd75692b205107ecf054a835288883505e0277
Sophia Katsuno <chiaki.katsuno@live.ca>
2023-05-29 23:55:17 -0700

    funciton arguments

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,15 @@
+//In that file, define a function named math that takes three arguments.  
+ // It's important for you to understand that arguments names are only used  
+  //to reference them.  
+
+  function math(x, y, z) {
+    return (y*z) + x
+  }
+  //Name each argument as you like.  
+   
+  console.log(math(53,61,67))
+  //Within the math function, return the value obtained from multiplying  
+  //the second and third arguments and adding that result to the first  
+  //argument.  
+   
+ 
